7aa25925b1cd54987e3c8d95a42722c16cd21597
Shubham13dj <shubhamjadhav1320@gmail.com>
2024-12-07 01:28:20 +0530

    First react app

---
+++ practiceapp/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {useState} from 'react';
+import Chai from './chai';
+
+function App() {
+  let [counter, SetCounter] = useState(5)
+  function addValue(){
+    console.log("value updated");
+    counter += 1;
+    SetCounter(counter);
+  }
+
+  function removeValue() {
+    if(counter === 0){
+      alert("can not remove now!!!");
+      return;
+    }
+    counter -= 1;
+    SetCounter(counter);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>React Counter</h1>
+       <p>Counter Value: {counter}</p>
+       <button onClick={addValue}>Add Counter</button>
+       <br/>
+       <button onClick={removeValue}>Remove Counter</button>
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
